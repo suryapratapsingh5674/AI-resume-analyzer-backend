1257651fd89a91338bbf9181f93e88b894685d14
surya pratap singh tomar <87597833+suryapratapsingh5674@users.noreply.github.com>
2026-04-09 10:20:51 +0530

    Update CORS origin to production URL

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "ai-resume-analyzer-frontend-nu.vercel.app",
     credentials: true,
   }),
 );
